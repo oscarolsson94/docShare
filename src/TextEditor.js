62426d66e546a756fb7ec8740d3c6a567074a468
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-22 21:33:54 +0200

    adding a text editor

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 
 const TextEditor = () => {
+
+    const wrapperRef = useRef();
+
+    useEffect(() => {
+        const editor = document.createElement("div");
+        wrapperRef.current.append(editor);
+        new Quill(editor, { theme: "snow" });
+
+        return () => {
+            wrapperRef.innerHTML = "";
+        }
+    }, []);
+
     return (
-        <div>
+        <div id="container" ref={wrapperRef} >
             
         </div>
     )
